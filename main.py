03798459b7b3979912a03633a916a81a1d6b14a0
vishalmunic17 <vqm5223@psu.edu>
2020-09-11 12:45:41 +0000

    periods

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def getGradePoint(GPA):
     return 2.33
   elif (GPA == "C"):
     return 2.0
+  elif (GPA == "D"):
+    return 1.0
   else: 
     return 0.0
 
@@ -20,21 +22,21 @@ def run():
   GPA = input("Enter your course 1 letter grade: ")
   CourseOne = getGradePoint(GPA)
   OneCredit = float(input("Enter your course 1 credit: "))
-  print(f"Grade point for course 1 is: {(getGradePoint(GPA))}.")
+  print(f"Grade point for course 1 is: {(getGradePoint(GPA))}")
 
   GPA = input("Enter your course 2 letter grade: ")
   CourseTwo = getGradePoint(GPA)
   TwoCredit = float(input("Enter your course 2 credit: "))
-  print(f"Grade point for course 2 is: {(getGradePoint(GPA))}.")
+  print(f"Grade point for course 2 is: {(getGradePoint(GPA))}")
 
   GPA = input("Enter your course 3 letter grade: ")
   ThreeCredit = float(input("Enter your course 3 credit: "))
   CourseThree = getGradePoint(GPA)
-  print(f"Grade point for course 3 is: {(getGradePoint(GPA))}.")
+  print(f"Grade point for course 3 is: {(getGradePoint(GPA))}")
 
   GPATotal = ((CourseOne * OneCredit) + (CourseTwo *TwoCredit) + (CourseThree *ThreeCredit) ) / (OneCredit + TwoCredit + ThreeCredit)
 
-  print(f"Your GPA is: {GPATotal}.")
+  print(f"Your GPA is: {GPATotal}")
 
 if __name__ == "__main__":
   run()
